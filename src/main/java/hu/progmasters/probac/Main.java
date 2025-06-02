0183dc3d438244574de53e09be12fb1d8fc83a00
BukkfalviZoltan <bukkfalviz@gmail.com>
2025-06-02 14:05:13 +0200

    Initial commit

---
+++ src/main/java/hu/progmasters/probac/Main.java
@@ -0,0 +1,13 @@
+package hu.progmasters.probac;
+
+public class Main {
+    /**
+     * Client code that uses Morse class from probac.
+     * @param args default arguments.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Morse morse = new Morse();
+        System.out.println(morse.ConvertText2Morse("ABA BABA KRUMPLI"));
+    }
+}
